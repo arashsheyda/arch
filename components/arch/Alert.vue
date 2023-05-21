7dcbab0d2802ae6e3122b026143b880c15f089f4
arashsheyda <sheidaeearash1999@gmail.com>
2023-05-21 18:54:35 +0300

    feat: alert component

---
+++ components/arch/Alert.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'info',
+  },
+})
+
+const alertTypes = {
+  info: 'alert-cyan',
+  warning: 'alert-yellow',
+  danger: 'alert-red',
+  success: 'alert-green',
+} as any
+
+const alertType = computed(() => {
+  return alertTypes[props.type] || 'alert-cyan'
+})
+</script>
+
+<template>
+  <div :class="alertType">
+    <slot />
+  </div>
+</template>
